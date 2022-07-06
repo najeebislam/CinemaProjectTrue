5d320e7674d085fe6026bd2e786c2cacc139545b
NI211044 <NI211044@reigate.ac.uk>
2022-07-06 13:12:05 +0100

    first working buttons

--- src/main/java/com/example/cinemaprojecttrue/HelloController.java
+++ src/main/java/com/example/cinemaprojecttrue/HelloController.java
@@ -2,8 +2,11 @@ package com.example.cinemaprojecttrue;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -16,9 +19,69 @@ public class HelloController {
 
 
     @FXML
-    Button Register;
+    Button Register,Return,Login,ReturnToOptions;
 
-    public void handleRegister() throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registerscreen.fxml"));
+
+    public void handleRegister() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registerscreen.fxml"));
+            //root = FXMLLoader.load(getClass().getResource("registerscreen.fxml"));
+
+            Stage window = (Stage) Register.getScene().getWindow();
+            window.setScene(new Scene(fxmlLoader.load(), 600, 450));
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+
+
     }
+
+
+    public void handleLogin() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginscreen.fxml"));
+            //root = FXMLLoader.load(getClass().getResource("registerscreen.fxml"));
+
+            Stage window = (Stage) Login.getScene().getWindow();
+            window.setScene(new Scene(fxmlLoader.load(), 600, 450));
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
+
+
+
+    public void handleReturn() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("openscreen.fxml"));
+            //root = FXMLLoader.load(getClass().getResource("registerscreen.fxml"));
+
+            Stage window = (Stage) Return.getScene().getWindow();
+            window.setScene(new Scene(fxmlLoader.load(), 600, 450));
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
+
+
+
+    public void handleReturnToOptions() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("openscreen.fxml"));
+            //root = FXMLLoader.load(getClass().getResource("registerscreen.fxml"));
+
+            Stage window = (Stage) ReturnToOptions.getScene().getWindow();
+            window.setScene(new Scene(fxmlLoader.load(), 600, 450));
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
+
+
 }
